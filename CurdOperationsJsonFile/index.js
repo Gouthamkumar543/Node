8473d6d335da05b6922096870c40525093f1030d
Gouthamkumar543 <gouthamkumar0345@gmail.com>
2025-04-08 19:36:23 +0530

    Put method done

--- CurdOperationsJsonFile/index.js
+++ CurdOperationsJsonFile/index.js
@@ -13,7 +13,7 @@
 
 // server.listen(4000,()=>{
 //     console.log("server running on http://localhost:4000")
-    
+
 // })
 
 
@@ -22,68 +22,98 @@ const http = require("http")
 const url = require("url")
 const fs = require("fs");
 
-function ReadFile(){
-    const data = fs.readFileSync("data.json","utf-8")
+function ReadFile() {
+    const data = fs.readFileSync("data.json", "utf-8")
     return JSON.parse(data)
 }
 
-function WriteFile(users){
-    fs.writeFileSync("data.json",JSON.stringify(users),"utf-8")
+function WriteFile(users) {
+    fs.writeFileSync("data.json", JSON.stringify(users), "utf-8")
 }
 
 
-const server = http.createServer((req,res)=>{
-    const parsedUrl = url.parse(req.url,true)
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true)
     const pathname = parsedUrl.pathname
     const query = parsedUrl.query
 
-    if(pathname === "/"){
-        res.writeHead(200,{"content-type":"text/plain"})
+    if (pathname === "/") {
+        res.writeHead(200, { "content-type": "text/plain" })
         res.write("Welcome to server")
         return res.end()
     }
 
-    if(pathname === "/users" && req.method === "GET"){
-        res.writeHead(200,{"content-type":"application/json"})
+    if (pathname === "/users" && req.method === "GET") {
+        res.writeHead(200, { "content-type": "application/json" })
         const User = ReadFile()
         res.write(JSON.stringify(User))
         return res.end()
     }
 
-    if(pathname === "/users" && req.method === "POST"){
+    if (pathname === "/users" && req.method === "DELETE") {
+        if (query.id) {
+            const user = ReadFile()
+            const FilteredData = user.filter(x => x.id !== Number(query.id))
+            WriteFile(FilteredData)
+            res.writeHead(200, { "content-type": "text/plain" })
+            res.write("Deleted SuccessFully")
+            return res.end()
+        } else {
+            res.writeHead(400, { "content-type": "text/plain" })
+            res.write("Deleted unsucesss")
+            return res.end()
+        }
+    }
+
+    if (pathname === "/users" && req.method === "POST") {
         let body = ""
-        req.on("data",(chunk)=>{
+        req.on("data", (chunk) => {
             body += chunk
         })
-        req.on("end",()=>{
+        req.on("end", () => {
             const newUser = JSON.parse(body)
-            res.writeHead(200,{"content-type":"application/json"})
+            res.writeHead(200, { "content-type": "application/json" })
             let user = ReadFile()
             // user.push(newUser)
-            user = [...user,newUser]
+            user = [...user, newUser]
             newUser.id = user.length
             WriteFile(user)
-            res.write(JSON.stringify({message:"Added Sucessfully !!!",newUser}))
+            res.write(JSON.stringify({ message: "Added Sucessfully !!!", newUser }))
             return res.end()
         })
     }
 
-    if(pathname === "/users" && req.method === "DELETE"){
-        if(query.id){
-            const user = ReadFile()
-            const FilteredData = user.filter(x=>x.id !== Number(query.id))
-            WriteFile(FilteredData)
-            res.writeHead(200,{"content-type":"text/plain"})
-            res.write("Deleted SuccessFully")
-            return res.end() 
+    if (pathname === "/users" && req.method === "PUT") {
+        if (query.id) {
+            let body = ""
+            req.on("data", (chunk) => {
+                body += chunk
+            })
+            req.on("end", () => {
+                let PutData = JSON.parse(body)
+                let user = ReadFile()
+                // console.log(user)                
+                const index = user.findIndex((x) => x.id === Number(query.id))
+                console.log(index)
+                if (index == -1) {
+                    res.writeHead(404, { "content-type": "text/plain" })
+                    res.write("No User Found")
+                    return res.end()
+                }
+                user[index] = PutData
+                WriteFile(user)
+                res.writeHead(201, { "content-type": "application/json" })
+                res.write(JSON.stringify({ message: "Updated Sucessfully", PutData }))
+                return res.end()
+            })
         }else{
-            res.writeHead(400,{"content-type":"text/plain"})
-            res.write("Deleted un sucesss")
+            res.writeHead(404,{"content-type":"text/plain"})
+            res.write("Invalid User ID")
             return res.end()
         }
     }
 })
 
-server.listen(4000,()=>{
+server.listen(4000, () => {
     console.log("server Running http://localhost:4000")
 })

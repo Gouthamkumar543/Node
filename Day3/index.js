09b5bce90cd298fc3846a07ef46eec75a00d08eb
Gouthamkumar543 <gouthamkumar0345@gmail.com>
2025-10-11 18:22:03 +0530

    Curd opertaions completed

--- Day3/index.js
+++ Day3/index.js
@@ -1,6 +1,33 @@
 const http = require("http")
 const url = require("url")
 
+let data = [{
+    id: 1,
+    title: "phone",
+    color: "black"
+},
+{
+    id: 2,
+    title: "phone",
+    color: "sliver"
+}, {
+    id: 3,
+    title: "tv",
+    color: "black"
+}, {
+    id: 4,
+    title: "sofa",
+    color: "brown"
+}, {
+    id: 5,
+    title: "phone",
+    color: "sliver"
+}, {
+    id: 6,
+    title: "tv",
+    color: "grey"
+}]
+
 const server = http.createServer(async (req, res) => {
     const parsedUrl = url.parse(req.url, true)
     const query = parsedUrl.query
@@ -11,65 +38,44 @@ const server = http.createServer(async (req, res) => {
         res.write("Welcome to the online products")
         return res.end()
     } else if (pathName === "/products") {
-        const data = [{
-            id: "1",
-            title: "phone",
-            color: "black"
-        },
-        {
-            id: "2",
-            title: "phone",
-            color: "sliver"
-        }, {
-            id: "3",
-            title: "tv",
-            color: "black"
-        }, {
-            id: "4",
-            title: "sofa",
-            color: "brown"
-        }, {
-            id: "5",
-            title: "phone",
-            color: "sliver"
-        }, {
-            id: "6",
-            title: "tv",
-            color: "grey"
-        }]
+        if (req.method === "GET") {
+            res.writeHead(200, { "content-type": "application/json" })
+            res.write(JSON.stringify(data))
+            return res.end()
+        }
 
-        // if (req.method === "GET" && query.id) {
-        //     const filterItem = data.filter(x => x.id === Number(query.id))
-        //     if (filterItem.length > 0) {
-        //         res.writeHead(200, { "content-type": "application/json" })
-        //         res.write(JSON.stringify(filterItem))
-        //         return res.end()
-        //     } else {
-        //         res.writeHead(404, { "content-type": "text/plain" })
-        //         res.write("No item found with the id")
-        //         return res.end()
-        //     }
-        // }
+        if (req.method === "GET" && query.id) {
+            const filterItem = data.filter(x => x.id === Number(query.id))
+            if (filterItem.length > 0) {
+                res.writeHead(200, { "content-type": "application/json" })
+                res.write(JSON.stringify(filterItem))
+                return res.end()
+            } else {
+                res.writeHead(404, { "content-type": "text/plain" })
+                res.write("No item found with the id")
+                return res.end()
+            }
+        }
 
-        // if (req.method === "GET" && query.category) {
-        //     const filterItem = data.filter(x => x.category === query.category)
-        //     if (filterItem.length > 0) {
-        //         res.writeHead(200, { "content-type": "application/json" })
-        //         res.write(JSON.stringify(filterItem))
-        //         return res.end()
-        //     } else {
-        //         res.writeHead(404, { "content-type": "text/plain" })
-        //         res.write("No item found with the category")
-        //         return res.end()
-        //     }
-        // }
+        if (req.method === "GET" && query.category) {
+            const filterItem = data.filter(x => x.category === query.category)
+            if (filterItem.length > 0) {
+                res.writeHead(200, { "content-type": "application/json" })
+                res.write(JSON.stringify(filterItem))
+                return res.end()
+            } else {
+                res.writeHead(404, { "content-type": "text/plain" })
+                res.write("No item found with the category")
+                return res.end()
+            }
+        }
 
         if (req.method === "DELETE") {
             if (query.id) {
                 const originalLength = data.length
                 const updatedData = data.filter((x) => x.id !== Number(query.id))
-                console.log(updatedData);
-                
+                // console.log(updatedData);
+
                 if (updatedData.length < originalLength) {
                     res.writeHead(200, { "content-type": "application/json" })
                     res.write(JSON.stringify({ message: "Item Deleted Sucessfully", updatedData }))
@@ -84,21 +90,32 @@ const server = http.createServer(async (req, res) => {
 
         if (req.method === "POST") {
             let body = ""
-            res.on("data", (chuncks) => {
+            req.on("data", (chuncks) => {
                 body += chuncks
-                console.log(body);
+                // console.log(body);
             })
-            res.on("end", () => {
-                let putData = body.parse()
-                res.writeHead(200, { "content-type": "application/json" })
-                res.write(JSON.stringify({ message: "data added sucessfully", putData }))
+
+            req.on("end", () => {
+                let postData = JSON.parse(body)
+                res.writeHead(201, { "content-type": "application/json" })
+                res.write(JSON.stringify({ message: "data added sucessfully", postData }))
                 return res.end()
             })
         }
 
-        res.writeHead(200, { "content-type": "application/json" })
-        res.write(JSON.stringify(data))
-        res.end()
+        if (req.method === "PUT") {
+            let body = ""
+            req.on("data", (chuncks) => {
+                body += chuncks
+            })
+
+            req.on("end", () => {
+                let putData = JSON.parse(body)
+                res.writeHead(201, { "content-type": "application/json" })
+                res.write(JSON.stringify({ message: "data updated sucessfully", putData }))
+                return res.end()
+            })
+        }
     }
     else {
         res.writeHead(200, { "content-type": "text/plain" })
